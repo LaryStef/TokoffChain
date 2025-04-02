52b7502318a0909e6b9be09b6311f2b8f6786e2c
Tim <timurkotov1999@gmail.com>
2025-04-02 17:14:03 +0300

    feat: Add response to transaction endpoint

--- app/api/routes/transactions.py
+++ app/api/routes/transactions.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 
 from app.api.schemes import SuccessResponse
+from app.api.schemes.transaction import Transaction
 
 router = APIRouter(prefix="/transactions", tags=["transactions"])
 
@@ -23,11 +24,19 @@ router = APIRouter(prefix="/transactions", tags=["transactions"])
                 "status": status.HTTP_201_CREATED,
                 "message": (
                     "Transaction successfully added to the list"
-                    "to be saved in next block",
+                    "to be saved in the next block",
                 ),
             },
         },
     },
 )
-async def create_transaction() -> JSONResponse:
-    return JSONResponse(content="")
+async def create_transaction(transaction: Transaction) -> JSONResponse:
+    return JSONResponse(
+        content=SuccessResponse(
+            status=status.HTTP_201_CREATED,
+            message=(
+                "Transaction successfully added to the list"
+                "to be saved in the next block"
+            ),
+        ).model_dump_json(),
+    )
