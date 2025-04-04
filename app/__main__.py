790450e8124169e8b2b4006a3c876494e6544606
lary_stef <timurkotov1999@gmail.com>
2025-04-04 16:03:46 +0300

    feat: App lifespan

--- app/__main__.py
+++ app/__main__.py
@@ -1,10 +1,31 @@
+import contextlib
+from collections.abc import AsyncGenerator
+
 import uvicorn
 from fastapi import FastAPI
 
 from app.api import api
+from app.core.database.database import database_manager
 from app.settings import settings
 
-app = FastAPI()
+
+@contextlib.asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
+    await database_manager.create_tables()
+    yield
+    await database_manager.shutdown()
+
+if settings.ENVIRONMENT == "development":
+    debug = True
+
+app = FastAPI(
+    debug=settings.DEBUG,
+    title=settings.NAME,
+    summary="API for Tokoff Chain",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    lifespan=lifespan,
+)
 app.include_router(api)
 
 if __name__ == "__main__":
